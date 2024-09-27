50e608a36cff6faae4a3d2799d0ab0bdf07b0dfb
untypequicode <untypequicode@gmail.com>
2024-09-27 18:27:58 +0200

    Modification du help avec quelques autres changements

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,21 @@
 #include "lib/reader.hpp"
 #include "lib/maze.hpp"
 
+void help() {
+    std::cout << "Usage: ./main [-option] <instance_file>" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "-h : Show this help" << std::endl;
+    std::cout << "-v : Show version" << std::endl;
+    std::cout << "-r : Read the instance file" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
-        std::cout << "Usage: ./main <instance_file>" << std::endl;
+        help();
         return 1;
     }
-    int numberOfCommands = 0;
-    int numberOfNoCommands = 0;
+    int numberOfCommands = 0, numberOfNoCommands = 0;
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
             numberOfCommands++;
@@ -20,15 +27,13 @@ int main(int argc, char *argv[]) {
             numberOfNoCommands++;
         }
     }
-    if (numberOfCommands > 1) {
-        std::cout << "Usage: ./main <instance_file> [-option]" << std::endl;
+    if (numberOfCommands < 1) {
+        help();
         return 1;
     }
     char *commands[numberOfCommands];
     char *noCommands[numberOfNoCommands];
-    int j = 0;
-    int k = 0;
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1, j = 0, k = 0; i < argc; i++) {
         if (argv[i][0] == '-') {
             commands[j] = argv[i];
             j++;
@@ -46,35 +51,23 @@ int main(int argc, char *argv[]) {
     }
     for (int i = 0; i < numberOfCommands; i++) {
         if (strcmp(commands[i], "-h") == 0) {
-            std::cout << "Usage: ./main <instance_file> [-option]" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "-h : Show this help" << std::endl;
-            std::cout << "-v : Show version" << std::endl;
-            std::cout << "-r : Read the instance file" << std::endl;
-            return 0;
+            help();
         } else if (strcmp(commands[i], "-v") == 0) {
             std::cout << "Version 0.1" << std::endl;
-            return 0;
         } else if (strcmp(commands[i], "-r") == 0) {
+            if (numberOfNoCommands < 1) {
+                help();
+                return 1;
+            }
             for (int i = 0; i < numberOfNoCommands; i++) {
                 Maze a = Maze();
                 Maze *maze = &a;
                 read(noCommands[i], maze);
             }
-            return 0;
         } else {
             std::cout << "Usage: ./main <instance_file> [-option]" << std::endl;
             return 1;
         }
     }
-
-
-
-    // Cell cell1 = Cell(1, 2);
-    // std::cout << cell1.getX() << std::endl;
-    // cell1.setX(10);
-    // std::cout << cell1.getX() << std::endl;
-
-    // std::cout << "Hello, World!" << std::endl;
     return 0;
 }
