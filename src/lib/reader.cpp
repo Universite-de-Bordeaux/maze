7f3fac122edd7eca4c70996dfe4b1e10859a6d1f
lulu-froid <lucien.glasson@gmail.com>
2024-09-29 17:10:10 +0200

    le reader fonctionnne (normalement)

--- src/lib/reader.cpp
+++ src/lib/reader.cpp
@@ -2,23 +2,29 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <ostream>
+#include <sstream>
 #include "reader.hpp"
-
-// void print_file(std::string file_tab) {
-
-// }
 
 void read(std::string filename, Maze *maze) {
     std::fstream file;
     file.open(filename, std::ios_base::in);
     std::string line;
     getline(file, line);
-    maze->setWidthHeight(line[0], line[2]);
+    maze->setWidthHeight((int)line[0], (int)line[2]);
     getline(file, line);
     while (!file.eof() && line.size() > 0) {
-        std::cout << line << std::endl;
-        if (!maze->addWall((int)line[0], (int)line[2], (int)line[4] == 'H')) {
-            exit(EXIT_FAILURE);
+        int x, y;
+        std::istringstream iss(line);
+        char orientation;
+        if (iss >> x >> y >> orientation) {
+            if (orientation != 'H' && orientation != 'V') {
+                exit(1);
+            }
+            if (!maze->addWall(x, y, orientation == 'H')) {
+                exit(1);
+            }
+            std::cout << x << ' ' << y << ' ' << orientation << std::endl;
         }
         getline(file, line);
     }
