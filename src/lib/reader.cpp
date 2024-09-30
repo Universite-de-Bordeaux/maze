ee807a0a39f527d93746976dcd23dc2315e1eddd
lulu-froid <lucien.glasson@gmail.com>
2024-09-30 10:00:51 +0200

    commentaire plus jolie

--- src/lib/reader.cpp
+++ src/lib/reader.cpp
@@ -7,44 +7,43 @@
 #include "var.hpp"
 
 void read(std::string filename, Maze *maze) {
-    // ouverture du fichier ----------------------------------------------------------------------
+    // ouverture du fichier
     std::fstream file;
     file.open(filename, std::ios_base::in);
     std::string line;
-    // -------------------------------------------------------------------------------------------
 
-    // initialisation des variable ---------------------------------------------------------------
+    // initialisation des variable
     getline(file, line); // permet de lire une ligne
     int width, height;
     std::istringstream iss(line);
-    // -------------------------------------------------------------------------------------------
+
     if (iss >> width >> height) { // iss permet de récuperer la largeur et la hauteur
         maze->setWidthHeight(width, height);
         getline(file, line);
+
         while (!file.eof() && line.size() > 0) {
             int x, y;
             std::istringstream iss(line);
             char orientation;
+
             if (iss >> x >> y >> orientation) {
-                // vérification d'erreur ---------------------------------------------------------
+
+                // vérification d'erreur
                 if (orientation != 'H' && orientation != 'V') {
                     exit(MAZE_FILE_ERROR);
                 }
-                // -------------------------------------------------------------------------------
 
                 // ajout du mur en prenant en compte les potentielles erreurs renvoyer par addWall
                 if (!maze->addWall(x, y, orientation == 'H')) {
                     exit(MAZE_FILE_ERROR);
                 }
-                // -------------------------------------------------------------------------------
             }
             getline(file, line);
         }
     }
-    // gestion d'erreur --------------------------------------------------------------------------
+    // gestion d'erreur
     else {
         exit(MAZE_FILE_ERROR);
     }
-    // ------------------------------------------------------------------------------------------
     file.close();
 }
