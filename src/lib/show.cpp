82014889cefea30f7841ad03e47c85f9dc97ab43
untypequicode <untypequicode@gmail.com>
2024-09-30 19:40:49 +0200

    show.cpp Changement des couleurs du visualiseur et affichage au milieu de l'écran de la fenêtre

--- src/lib/show.cpp
+++ src/lib/show.cpp
@@ -1,49 +1,22 @@
 #include "show.hpp"
-#include <iostream>
+#include <SFML/Window/VideoMode.hpp>
 #include <SFML/Graphics.hpp>
 #include "var.hpp"
 
 void show(Maze *maze) {
-    // for (int x = 0; x < maze->getWidth() * 2 + 1; x++) {
-    //     std::cout << "_";
-    // }
-    // std::cout << std::endl;
-    // for (int y = 0; y < maze->getHeight() - 1; y++) {
-    //     std::cout << "|";
-    //     for (int x = 0; x < maze->getWidth() - 1; x++) {
-    //         Cell* cell = maze->getCell(x, y);
-    //         if (cell->getWall(MAZE_CELL_BOTTOM) != nullptr) {
-    //             std::cout << "_";
-    //         } else {
-    //             std::cout << " ";
-    //         }
-    //         if (cell->getWall(MAZE_CELL_RIGHT) != nullptr) {
-    //             std::cout << "|";
-    //         } else {
-    //             std::cout << " ";
-    //         }
-    //     }
-    //     if (maze->getCell(maze->getWidth() - 1, y)->getWall(MAZE_CELL_BOTTOM) != nullptr) {
-    //         std::cout << "_";
-    //     } else {
-    //         std::cout << " ";
-    //     }
-    //     std::cout << "|" << std::endl;
-    // }
-    // std::cout << "|";
-    // for (int x = 0; x < maze->getWidth() - 1; x++) {
-    //     Cell* cell = maze->getCell(x, maze->getHeight() - 1);
-    //     std::cout << "_";
-    //     if (cell->getWall(MAZE_CELL_RIGHT) != nullptr) {
-    //         std::cout << "|";
-    //     } else {
-    //         std::cout << "_";
-    //     }
-    // }
-    // std::cout << "_|" << std::endl;
-
     // Création de la fenêtre
-    sf::RenderWindow window(sf::VideoMode(maze->getWidth() * 20, maze->getHeight() * 20), "Maze");
+    sf::VideoMode desktopSize = sf::VideoMode::getDesktopMode();
+
+    int cellSize = 50;
+    if (maze->getWidth() * cellSize > desktopSize.width * .9 || maze->getHeight() * cellSize > desktopSize.height * .9) {
+        cellSize = std::min(desktopSize.width / maze->getWidth(), desktopSize.height / maze->getHeight()) * .9;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(maze->getWidth() * cellSize, maze->getHeight() * cellSize), "Maze");
+
+    // Positionnement de la fenêtre au centre de l'écran
+    sf::Vector2i screenCenter(desktopSize.width / 2, desktopSize.height / 2);
+    window.setPosition(screenCenter - sf::Vector2i(window.getSize().x / 2, window.getSize().y / 2));
 
     // Boucle principale
     while (window.isOpen()) {
@@ -55,38 +28,52 @@ void show(Maze *maze) {
         }
 
         // Dessin du labyrinthe
-        window.clear(sf::Color::White);
+        window.clear(sf::Color::Black);
         for (int y = 0; y < maze->getHeight(); y++) {
             for (int x = 0; x < maze->getWidth(); x++) {
                 Cell* cell = maze->getCell(x, y);
 
                 // Dessin des murs
                 if (cell->getWall(MAZE_CELL_BOTTOM)) {
-                    sf::RectangleShape wall(sf::Vector2f(20, 2));
-                    wall.setPosition(x * 20, y * 20 + 19);
-                    wall.setFillColor(sf::Color::Black);
+                    sf::RectangleShape wall(sf::Vector2f(cellSize, 2));
+                    wall.setPosition(x * cellSize, y * cellSize + cellSize - 2);
+                    wall.setFillColor(sf::Color::White);
                     window.draw(wall);
                 }
                 if (cell->getWall(MAZE_CELL_RIGHT)) {
-                    sf::RectangleShape wall(sf::Vector2f(2, 20));
-                    wall.setPosition(x * 20 + 19, y * 20);
-                    wall.setFillColor(sf::Color::Black);
+                    sf::RectangleShape wall(sf::Vector2f(2, cellSize));
+                    wall.setPosition(x * cellSize + cellSize - 2, y * cellSize);
+                    wall.setFillColor(sf::Color::White);
                     window.draw(wall);
                 }
             }
         }
 
         // Dessin des bordures
-        sf::RectangleShape border(sf::Vector2f(maze->getWidth() * 20, 1));
+        sf::RectangleShape border(sf::Vector2f(maze->getWidth() * cellSize, 1));
         border.setPosition(0, 0);
+        border.setFillColor(sf::Color::White);
+        window.draw(border);
+        border.setPosition(0, maze->getHeight() * cellSize - 1);
+        window.draw(border);
+        border.setSize(sf::Vector2f(1, maze->getHeight() * cellSize));
+        border.setPosition(0, 0);
+        window.draw(border);
+        border.setPosition(maze->getWidth() * cellSize - 1, 0);
+        window.draw(border);
         border.setFillColor(sf::Color::Red);
-        window.draw(border);
-        border.setPosition(0, maze->getHeight() * 20-1);
-        window.draw(border);
-        border.setSize(sf::Vector2f(1, maze->getHeight() * 20));
+        border.setSize(sf::Vector2f(1, cellSize));
         border.setPosition(0, 0);
         window.draw(border);
-        border.setPosition(maze->getWidth() * 20 - 1, 0);
+        border.setSize(sf::Vector2f(cellSize, 1));
+        border.setPosition(0, 0);
+        window.draw(border);
+        border.setFillColor(sf::Color::Green);
+        border.setSize(sf::Vector2f(1, cellSize));
+        border.setPosition(maze->getWidth() * cellSize - 1, maze->getHeight() * cellSize - cellSize);
+        window.draw(border);
+        border.setSize(sf::Vector2f(cellSize, 1));
+        border.setPosition(maze->getWidth() * cellSize - cellSize, maze->getHeight() * cellSize - 1);
         window.draw(border);
 
         // Affichage de la fenêtre
