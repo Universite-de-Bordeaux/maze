117c0477f830d6eb8ad487771d675fe23d6ff028
Lucien Glasson <lucien.glasson@gmail.com>
2024-11-08 16:40:00 +0100

    algo_fractal CMamkeLists.txt

--- src/lib/algo_fractal.cpp
+++ src/lib/algo_fractal.cpp
@@ -1,65 +1,82 @@
-# include "algo_fractal.hpp"
+#include "algo_fractal.hpp"
 #include "maze.hpp"
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 
-void add_vertical(Maze *maze, int height, int column) {
+static void add_column(Maze *maze, int column) {
     for (int i = 0; i < maze->getHeight(); i++) {
+        std::cout << column << ' ' << i << " vertical" << std::endl;
         maze->addWall(column, i, false);
     }
     return;
 }
 
-void add_horizontal(Maze *maze, int line, int width) {
+static void add_line(Maze *maze, int line) {
 
-    for (int i = 0; i < width; i++) {
-        std::cout << i << ' ' << line << std::endl;
-        std::cout << maze->addWall(i, line, true) << std::endl;
+    for (int i = 0; i < maze->getWidth(); i++) {
+        std::cout << i << ' ' << line << " horizontal" << std::endl;
+        maze->addWall(i, line, true);
     }
     return;
 }
 
 // void remove(Maze * maze, int direction, );
 
-void quad_maze(Maze * maze) {
+static void quad_maze(Maze * maze) {
     int old_width = maze->getWidth();
     int old_height = maze->getHeight();
-    maze->setWidthHeight(2*maze->getWidth(), 2*maze->getHeight());
-
-    for (int i = 0; i < old_height; i++) {
-        for (int j = 0; j < old_width; j++) {
-            maze->setCell(i, j+old_width, maze->getCell(i, j));
-            maze->setCell(i+old_height, j, maze->getCell(i, j));
-            maze->setCell(i+old_height, j+old_width, maze->getCell(i, j));
+    Maze new_maze = Maze();
+    new_maze.setWidthHeight(old_width, old_height);
+    // copie de maze dans new_maze
+    for (int i = 0; i < old_width; i++) {
+        for (int j = 0; j < old_height; j++) {
+            for (int k = 0; k < 2; k++) {
+                if (maze->getWall(i, j, k)) {
+                    new_maze.addWall(i, j, k);
+                }
+            }
         }
     }
-    return;
+    maze->setWidthHeight(2*maze->getWidth(), 2*maze->getHeight());
+    std::cout << maze->getWall(0, 0, false) << std::endl;
+    for (int i = 0; i < old_height; i++) {
+        for (int j = 0; j < old_width; j++) {
+            Cell *cell = new_maze.getCell(i, j);
+            for (int k = 0; k < 2; k++) {
+                if (maze->getWall(i, j, k)) {
+                    maze->addWall(i, j, k);
+                    maze->addWall(i+old_width, j, k);
+                    maze->addWall(i+old_width, j+old_height, k);
+                    maze->addWall(i, j+old_height, k);
+                }
+            }
+        }
+    }
+    new_maze.clearMaze();
 }
 
 void algo_fractal(Maze* maze, int n, bool perfect, Show *show) {
     int nb_murs_supp = 3;
     n = std::pow(2, n);
-    maze->setWidthHeight(2, 2);
-    while (n) {
+    maze->setWidthHeight(1, 1);
+    while (n > 1) {
+        std::cout << "MAZE ACTUEL : " << maze->getWidth() << ' ' << maze->getHeight() << std::endl;
         quad_maze(maze);
-        int *p_1 = &nb_murs_supp;
         int width = maze->getWidth();
         int height = maze->getHeight();
-        std::cout << "taille : " << width << ' ' << height << std::endl;
         int r = rand() % 4;
-        if (!perfect) {
-            *p_1 = 3 + r % 2;
-        }
+        // if (!perfect) {
+        //     *p_1 = 3 + r % 2;
+        // }
 
-        int line = height / 2;
-        int column = width / 2;
-        // std::cout << "test 1" << std::endl;
-        add_horizontal(maze, line, width);
-        // std::cout << "test 2" << std::endl;
-        add_vertical(maze, height, column);
-
+        int line = height / 2 - 1;
+        int column = width / 2 - 1;
+        add_line(maze, line);
+        add_column(maze, column);
+        // FIXME
         n /= 2;
+        std::cout << "n: " << n << std::endl;
     }
     return;
 }
