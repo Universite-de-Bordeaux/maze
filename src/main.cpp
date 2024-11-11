9fd266e4aea28f29b1260693d016efe4115ec35f
Aloyse ARNAUD <aloyse.arnaud@etu.u-bordeaux.fr>
2024-11-11 19:38:27 +0100

    Création du solver_width

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "lib/writer.hpp"
 #include "lib/checker.hpp"
 #include "lib/solver_1.hpp"
+#include "lib/solver_width.hpp"
 #include "lib/algo_fractal.hpp"
 #include <ctime>
 
@@ -95,6 +96,8 @@ void resolveMaze(Maze *maze, std::string algorithm, Show *show) {
     clock_t start = clock();
     if (algorithm == "1r") solver_1(maze, show, false);
     else if (algorithm == "1") solver_1(maze, show, true);
+    else if (algorithm == "wr") solver_width(maze, show, false);
+    else if (algorithm == "w") solver_width(maze, show, true);
     else exit(MAZE_COMMAND_ERROR);
     clock_t end = clock();
     std::cout << "Resolved in " << (double)(end - start) / CLOCKS_PER_SEC * 1000 << "ms" << std::endl;
@@ -304,6 +307,10 @@ int main(int argc, char *argv[]) {
                             algorithm = "1r";
                         } else if (strcmp(argv[i + 1], "1") == 0) {
                             algorithm = "1";
+                        } else if (strcmp(argv[i + 1], "wr") == 0) {
+                            algorithm = "wr";
+                        } else if (strcmp(argv[i + 1], "w") == 0) {
+                            algorithm = "w";
                         } else {
                             return help(MAZE_COMMAND_ERROR);
                         }
