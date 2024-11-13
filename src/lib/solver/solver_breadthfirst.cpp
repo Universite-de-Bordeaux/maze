6f96bf24d01d4bc3f1d6bd86a438e5603e04234b
untypequicode <ethan.facca@untypequicode.fr>
2024-11-13 02:52:14 +0100

    src/lib/solver/solver_breadthfirst.cpp Mise à jour du solver breadthfirst qui fonctionne toujours avec un programme beaucoup plus léger Il faut encore faire le système de retour en arrière

--- src/lib/solver/solver_breadthfirst.cpp
+++ src/lib/solver/solver_breadthfirst.cpp
@@ -1,52 +1,97 @@
 #include <queue>
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 #include "solver_breadthfirst.hpp"
 #include "../show.hpp"
 #include "../var.hpp"
 
 struct Position {
-    int x, y, direction;
+    int x;
+    int y;
+    int parentX;
+    int parentY;
+};
+Position *position;
+
+class QueuePosition {
+    private:
+        int size_;
+        int capacity_;
+    public:
+        QueuePosition() {
+            size_ = 0;
+            capacity_ = 1;
+            position = new Position[1];
+        }
+        ~QueuePosition() {
+            delete[] position;
+        }
+        void push(int x, int y, int parentX, int parentY) {
+            if (size_ == capacity_) {
+                Position *newPosition = new Position[capacity_*2];
+                for (int i = 0; i < size_; i++) {
+                    newPosition[i] = position[i];
+                }
+                delete[] position;
+                position = newPosition;
+                capacity_ *= 2;
+            }
+            position[size_].x = x;
+            position[size_].y = y;
+            position[size_].parentX = parentX;
+            position[size_].parentY = parentY;
+            size_++;
+        }
+        void pop() {
+            if (size_ < capacity_/4) {
+                Position *newPosition = new Position[capacity_/2];
+                for (int i = 0; i < size_; i++) {
+                    newPosition[i] = position[i];
+                }
+                delete[] position;
+                position = newPosition;
+                capacity_ /= 2;
+            }
+            if (size_ > 0) {
+                for (int i = 0; i < size_-1; i++) {
+                    position[i] = position[i+1];
+                }
+                size_--;
+            }
+        }
+        Position front() {
+            return position[0];
+        }
+        bool empty() {
+            return size_ == 0;
+        }
 };
 
-static bool solver_bfs(Maze *maze, Show *show, bool toLeft) {
-    std::queue<Position> q;
-    Cell *startCell = maze->getCell(maze->getStartX(), maze->getStartY());
-    startCell->setAlreadyVisited(true);
-    startCell->setStatus(MAZE_STATUS_CURRENT);
-
-    q.push({maze->getStartX(), maze->getStartY(), 0});
-    updateShowLive(show, maze, 1, &startCell);
-
-    while (!q.empty()) {
-        refreshShow(show);
-        Position pos = q.front();
-        q.pop();
-        Cell *cell = maze->getCell(pos.x, pos.y);
-        if (pos.x == maze->getEndX() && pos.y == maze->getEndY()) {
+bool solver_bfs(Maze *maze, Show *show, QueuePosition &queue) {
+    if (maze->getStartCell() == nullptr || maze->getEndCell() == nullptr) {
+        return false;
+    }
+    queue.push(maze->getStartX(), maze->getStartY(), -1, -1);
+    maze->getStartCell()->setStatus(MAZE_STATUS_VISITED);
+    while (!queue.empty()) {
+        Position current = queue.front();
+        queue.pop();
+        int x = current.x;
+        int y = current.y;
+        Cell *cell = maze->getCell(x, y);
+        if (cell->getX() == maze->getEndX() && cell->getY() == maze->getEndY()) {
             cell->setStatus(MAZE_STATUS_WAY_OUT);
-            updateShowLive(show, maze, 1, &cell);
+            updateShowLive(show, maze);
             return true;
         }
-
-        // Exploration des voisins
+        updateShowLive(show, maze);
         for (int i = 0; i < 4; i++) {
-            int index = (toLeft) ? (pos.direction + i) % 4 : (pos.direction - i) % 4;
-            if (index < 0) {
-                index += 4;
-            }
-            if (cell->getWall(index)) {
-                Cell *neighbor = cell->getNeighbor(index);
-                if (neighbor != nullptr && !neighbor->isAlreadyVisited()) {
-                    neighbor->setAlreadyVisited(true);
-                    neighbor->setStatus(MAZE_STATUS_HOPELESS);
-                    updateShowLive(show, maze, 1, &neighbor);
-
-                    q.push({neighbor->getX(), neighbor->getY(), (index + 2) % 4});
-
-                    cell->setStatus(MAZE_STATUS_VISITED);
-                    updateShowLive(show, maze, 1, &cell);
-                }
+            Cell *neighbor = cell->getNeighbor(i);
+            if (neighbor != nullptr && neighbor->getStatus() == MAZE_STATUS_IDLE) {
+                queue.push(neighbor->getX(), neighbor->getY(), x, y);
+                neighbor->setStatus(MAZE_STATUS_VISITED);
             }
         }
     }
@@ -56,5 +101,6 @@ static bool solver_bfs(Maze *maze, Show *show, bool toLeft) {
 bool solver_breadthfirst(Maze *maze, Show *show) {
     updateShowLive(show, maze);
     std::cout << "Résolution du labyrinthe en largeur" << std::endl;
-    return solver_bfs(maze, show, true);
+    QueuePosition queue;
+    return solver_bfs(maze, show, queue);
 }
