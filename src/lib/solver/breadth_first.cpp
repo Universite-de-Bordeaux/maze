9b4eb914f3d9104f505d07f44b12eb73b480c817
untypequicode <ethan.facca@untypequicode.fr>
2025-02-10 22:01:11 +0100

    breadth_first.cpp Optimisation du code permettant de meilleurs temps et moins d'empreinte mémoire

--- src/lib/solver/breadth_first.cpp
+++ src/lib/solver/breadth_first.cpp
@@ -2,82 +2,58 @@
 
 #include "../queue.hpp"
 #include "../show.hpp"
-#include "../stack.hpp"
 #include "../var.hpp"
-
-struct position {
-    int x;
-    int y;
-};
 
 struct positionHistory {
     int x;
     int y;
-    int parent_x;
-    int parent_y;
+    positionHistory *parent;
 };
 
 bool solver_breadth_first(const Maze *maze, Show *show) {
     Queue queue;
-    Stack stack;
     if (maze->getStartCell() == nullptr || maze->getEndCell() == nullptr) {
         return false;
     }
     refreshShow(show);
-    position start = {maze->getStartX(), maze->getStartY()};
-    queue.push(&start);
-    positionHistory startHistory = {maze->getStartX(), maze->getStartY(), -1,
-                                    -1};
-    stack.push(&startHistory);
+    positionHistory startHistory = {maze->getStartX(), maze->getStartY(),
+                                    nullptr};
+    queue.push(&startHistory);
     maze->getStartCell()->setStatus(MAZE_STATUS_VISITED);
     maze->getStartCell()->setAlreadyVisited(true);
     while (!queue.empty()) {
-        const auto *current = static_cast<position *>(queue.front());
+        auto *current = static_cast<positionHistory *>(queue.front());
         queue.pop();
         const int x = current->x;
         const int y = current->y;
         Cell *cell = maze->getCell(x, y);
         refreshShow(show, 1, &cell, false);
+
         int count = 0;
         for (int i = 0; i < 4; i++) {
             Cell *neighbor = cell->getNeighbor(i);
             if (neighbor != nullptr && !neighbor->isAlreadyVisited()) {
-                auto *neighborPosition = new position;
-                neighborPosition->x = neighbor->getX();
-                neighborPosition->y = neighbor->getY();
-                queue.push(neighborPosition);
-                auto *neighborHistory = new positionHistory;
-                neighborHistory->x = neighbor->getX();
-                neighborHistory->y = neighbor->getY();
-                neighborHistory->parent_x = x;
-                neighborHistory->parent_y = y;
-                stack.push(neighborHistory);
+                auto *next = new positionHistory;
+                next->x = neighbor->getX();
+                next->y = neighbor->getY();
+                next->parent = current;
+                queue.push(next);
                 neighbor->setStatus(MAZE_STATUS_VISITED);
                 neighbor->setAlreadyVisited(true);
                 if (neighbor->getX() == maze->getEndX() &&
                     neighbor->getY() == maze->getEndY()) {
                     neighbor->setStatus(MAZE_STATUS_WAY_OUT);
                     refreshShow(show, 1, &neighbor, true);
-                    while (!stack.empty()) {
-                        const auto *currentCell =
-                            static_cast<positionHistory *>(stack.top());
-                        const auto *cellTop =
-                            static_cast<positionHistory *>(stack.top());
-                        while (!stack.empty() &&
-                               (cellTop->x != currentCell->parent_x ||
-                                cellTop->y != currentCell->parent_y)) {
-                            stack.pop();
-                            cellTop =
-                                static_cast<positionHistory *>(stack.top());
+
+                    const auto *currentPath = next;
+                    while (currentPath != nullptr) {
+                        Cell *cellTop =
+                            maze->getCell(currentPath->x, currentPath->y);
+                        if (cellTop != nullptr) {
+                            cellTop->setStatus(MAZE_STATUS_WAY_OUT);
+                            refreshShow(show, 1, &cellTop, false);
                         }
-                        if (stack.empty()) {
-                            break;
-                        }
-                        cell = maze->getCell(cellTop->x, cellTop->y);
-                        if (cell != nullptr) {
-                            cell->setStatus(MAZE_STATUS_WAY_OUT);
-                            refreshShow(show, 1, &cell, false);
-                        }
+                        currentPath = currentPath->parent;
                     }
                     refreshShow(show);
                     return true;
