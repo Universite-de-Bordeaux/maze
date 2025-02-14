5874261520a6ad27a0f37a0360f680bcb5d43340
untypequicode <ethan.facca@untypequicode.fr>
2025-02-14 14:08:02 +0100

    wall_maker.cpp Utilisation d'une stack

--- src/lib/algo/wall_maker.cpp
+++ src/lib/algo/wall_maker.cpp
@@ -5,6 +5,7 @@
 #include "../checker/depth_first.hpp"
 #include "../queue.hpp"
 #include "../show.hpp"
+#include "../stack.hpp"
 
 struct wall_maker {
     int x;
@@ -94,14 +95,14 @@ static void resetAlreadyVisited(const Maze* maze) {
     }
 }
 
-void algo_wall_maker(Maze* maze, const int width, const int height, bool,
-                     double, Show* show) {
+void algo_wall_maker(Maze* maze, const int width, const int height,
+                     bool perfect, double probability, Show* show) {
     maze->setWidthHeight(width, height);
     if (show) {
         show->create();
     }
     refreshShow(show);
-    Queue queue;
+    Stack stack;
     for (int x = 0; x < width; x++) {
         for (int y = 0; y < height; y++) {
             for (int i = 0; i < 2; i++) {
@@ -110,33 +111,32 @@ void algo_wall_maker(Maze* maze, const int width, const int height, bool,
                     wall->x = x;
                     wall->y = y;
                     wall->horizontal = false;
-                    queue.push(wall);
+                    stack.push(wall);
                 } else if (i == 1 && y < height - 1) {
                     auto* wall = new wall_maker;
                     wall->x = x;
                     wall->y = y;
                     wall->horizontal = true;
-                    queue.push(wall);
+                    stack.push(wall);
                 }
             }
         }
     }
-    // std::cout << "wallsPossibleSize=" << wallsPossibleSize << std::endl;
 
-    while (!queue.empty()) {
+    while (!stack.empty()) {
         const auto* wallsPossible =
-            static_cast<wall_maker*>(queue.pop(maze->getRand()));
+            static_cast<wall_maker*>(stack.pop(maze->getRand()));
         const int x = wallsPossible->x;
         const int y = wallsPossible->y;
         const bool direction = wallsPossible->horizontal;
         maze->addWall(x, y, direction);
         // if (number > 1) {
-        bool isValid = false;
-        checker_depth_first(maze, false, false, nullptr, &isValid, nullptr);
-        if (!isValid) {
-            maze->removeWall(x, y, direction);
-        }
-        maze->clearMaze();
+        // bool isValid = false;
+        // checker_depth_first(maze, false, false, nullptr, &isValid, nullptr);
+        // if (!isValid) {
+        //     maze->removeWall(x, y, direction);
+        // }
+        // maze->clearMaze();
         // resetAlreadyVisited(maze);
         Cell* showCell[1] = {maze->getCell(x, y)};
         refreshShow(show, 1, showCell);
