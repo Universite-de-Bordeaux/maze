f51fc586530be89c957acafac7c293c87ca4b49a
untypequicode <ethan.facca@untypequicode.fr>
2025-02-19 19:08:00 +0100

    main_data.cpp Initialisation avec un help

--- src/main_data.cpp
+++ src/main_data.cpp
@@ -1,1 +1,57 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "lib/var.hpp"
+
+/**
+ * Affiche la documentation d'aide pour l'application de génération
+ */
+void help() {
+    std::cout << "============================================================="
+                 "====\n";
+    std::cout << "=                        MAZE DATA APPLICATION              "
+                 "      =\n";
+    std::cout << "============================================================="
+                 "====\n";
+    std::cout << "\n";
+
+    std::cout << "Usage : ./maze_generator.out [-option] [arguments]\n";
+    std::cout << "\n";
+
+    std::cout << "OPTIONS\n";
+    std::cout << "------------------------------\n";
+    std::cout << "-h, --help                    Affiche cette documentation\n";
+
+    std::cout << "Pour plus d'informations, veuillez consulter la "
+                 "documentation complète.\n";
+}
+/**
+ * Affiche l'aide et retourne le code d'erreur
+ * @param error Code d'erreur
+ * @return Code d'erreur
+ */
+int help(const int error) {
+    help();
+    return error;
+}
+
+/**
+ * Affiche l'aide et retourne le code d'erreur
+ * @param error Code d'erreur
+ * @param command Commande
+ * @return Code d'erreur
+ */
+int help(const int error, const std::string &command) {
+    std::cout << "Error : " << command << std::endl;
+    return help(error);
+}
+
+/**
+ * Main
+ * @param argc Nombre d'arguments
+ * @param argv Arguments
+ * @return Code d'erreur
+ */
+int main(const int argc, char *argv[]) {
+    if (argc < 2) return help(MAZE_COMMAND_ERROR);
+    return 0;
+}
