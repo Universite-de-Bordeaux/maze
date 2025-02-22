4b58da3dc1ae4b8c32fa8a82dc3a9373fa91c976
untypequicode <ethan.facca@untypequicode.fr>
2025-02-22 16:17:27 +0100

    dead_end_hand.cpp Finalisation de l'algorithme cul-de-sac pour la version main droite ou gauche

--- src/lib/game/dead_end_hand.cpp
+++ src/lib/game/dead_end_hand.cpp
@@ -6,6 +6,10 @@
 int game_dead_end_hand(const Maze *maze, Show *show, const bool left) {
     Cell *cell = maze->getCell(maze->getStartX(), maze->getStartY());
     cell->setStatus(MAZE_STATUS_CURRENT);
+    if (cell->getAbsoluteNumberOfNeighborsNotVisited() <= 1) {
+        cell->setStatus(MAZE_STATUS_HOPELESS);
+        cell->setAlreadyVisited(true);
+    }
     refreshShow(show);
     int direction = 0;
     const int move = left ? 1 : 3;
@@ -16,18 +20,24 @@ int game_dead_end_hand(const Maze *maze, Show *show, const bool left) {
     while (
         (cell->getX() != maze->getEndX() || cell->getY() != maze->getEndY()) &&
         steps <= pow(maze->getWidth() * maze->getHeight(), 3)) {
-        const int nbNeighbors = cell->getAbsoluteNumberOfNeighbors();
-        if (nbNeighbors == 0) {
+        int nbNeighborsNotVisited =
+            cell->getAbsoluteNumberOfNeighborsNotVisited();
+        if (nbNeighborsNotVisited == 0) {
             cell->setStatus(MAZE_STATUS_HOPELESS);
             refreshShow(show, 1, &cell);
             return -1;
         }
-        for (int i = 0; i < 3 && cell->getNeighbor(direction) == nullptr; i++) {
+        for (int i = 0;
+             i < 3 && (cell->getNeighbor(direction) == nullptr ||
+                       cell->getNeighbor(direction)->isAlreadyVisited());
+             i++) {
             direction = (direction + move) % 4;
         }
         if (tmpChangeDirection) {
             int tmpDirection = (direction + 2) % 4;
-            for (int i = 0; i < 3 && cell->getNeighbor(tmpDirection) == nullptr;
+            for (int i = 0;
+                 i < 3 && (cell->getNeighbor(tmpDirection) == nullptr ||
+                           cell->getNeighbor(tmpDirection)->isAlreadyVisited());
                  i++) {
                 tmpDirection = (tmpDirection + move + 2) % 4;
             }
@@ -41,10 +51,15 @@ int game_dead_end_hand(const Maze *maze, Show *show, const bool left) {
         if (direction < 0) direction += 4;
         if (neighbor != nullptr) {
             neighbor->setStatus(MAZE_STATUS_CURRENT);
-            cell->setStatus(MAZE_STATUS_VISITED);
+            if (cell->getStatus() != MAZE_STATUS_HOPELESS)
+                cell->setStatus(MAZE_STATUS_VISITED);
             Cell *showCell[2] = {cell, neighbor};
             refreshShow(show, 2, showCell);
             cell = neighbor;
+        }
+        if (cell->getAbsoluteNumberOfNeighborsNotVisited() <= 1) {
+            cell->setStatus(MAZE_STATUS_HOPELESS);
+            cell->setAlreadyVisited(true);
         }
         steps++;
         count++;
