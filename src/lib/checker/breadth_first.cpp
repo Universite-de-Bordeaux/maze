5388658c8df8e94ffe76bab293b30dcca49d8f1a
untypequicode <ethan.facca@untypequicode.fr>
2025-02-27 21:12:18 +0100

    Ajout des commentaires pour les algorithmes de verification de labyrinthe

--- src/lib/checker/breadth_first.cpp
+++ src/lib/checker/breadth_first.cpp
@@ -4,6 +4,7 @@
 #include "../show.hpp"
 #include "../var.hpp"
 
+// Structure pour stocker l'historique des positions visitées et leur parent
 struct positionHistory {
     int x;
     int y;
@@ -12,59 +13,81 @@ struct positionHistory {
 
 void checker_breadth_first(const Maze *maze, const bool perfect, Show *show,
                            bool *isValid, bool *isPerfect) {
-    Queue queue;
-    Queue queueFree;
-    bool imperfect = false;
-    refreshShow(show);
+    // Initialisation des structures de données
+    Queue queue;      // File d'attente pour le parcours BFS
+    Queue queueFree;  // File d'attente pour gérer la libération de la mémoire
+    bool imperfect =
+        false;          // Drapeau pour vérifier si le labyrinthe est parfait
+    refreshShow(show);  // Rafraîchissement de l'affichage
+
+    // Initialisation du point de départ
     Cell *start = maze->getCell(0, 0);
     positionHistory startHistory = {0, 0, nullptr};
-    queue.push(&startHistory);
-    start->setStatus(MAZE_STATUS_VISITED);
+    queue.push(
+        &startHistory);  // Ajout de la position initiale à la file d'attente
+    start->setStatus(
+        MAZE_STATUS_VISITED);  // Marquage de la cellule de départ comme visitée
     start->setAlreadyVisited(true);
 
+    // Parcours en largeur (BFS)
     while (!queue.empty()) {
+        // Extraction de l'élément devant la file d'attente
         auto *current = static_cast<positionHistory *>(queue.front());
         queue.pop();
         const int x = current->x;
         const int y = current->y;
         Cell *cell = maze->getCell(x, y);
+
+        // Vérification si le labyrinthe n'est pas parfait
         if (perfect && cell->getAbsoluteNumberOfNeighbors() -
                                cell->getAbsoluteNumberOfNeighborsNotVisited() >=
                            2) {
             cell->setStatus(MAZE_STATUS_TOO_MANY_NEIGHBORS);
             imperfect = true;
         }
+
+        // Vérification des voisins non visités
         if (cell->getAbsoluteNumberOfNeighborsNotVisited() == 0) {
             if (cell->getAbsoluteNumberOfNeighbors() -
                     cell->getAbsoluteNumberOfNeighborsNotVisited() <
                 2) {
                 cell->setStatus(MAZE_STATUS_HOPELESS);
             }
-            refreshShow(show, 1, &cell, true);
+            refreshShow(show, 1, &cell,
+                        true);  // Mise à jour de l'affichage pour cette cellule
         } else {
-            refreshShow(show, 1, &cell, false);
+            refreshShow(show, 1, &cell,
+                        false);  // Mise à jour normale de l'affichage
         }
+
+        // Exploration des voisins
         for (int i = 0; i < 4; i++) {
             Cell *neighbor = cell->getNeighbor(i);
             if (neighbor != nullptr && !neighbor->isAlreadyVisited()) {
-                auto *next = new positionHistory;
+                auto *next = new positionHistory;  // Création d'un nouvel
+                                                   // élément d'historique
                 next->x = neighbor->getX();
                 next->y = neighbor->getY();
                 next->parent = current;
-                queue.push(next);
-                queueFree.push(next);
-                neighbor->setStatus(MAZE_STATUS_VISITED);
+                queue.push(next);  // Ajout à la file d'attente principale
+                queueFree.push(
+                    next);  // Ajout à la file d'attente pour la libération
+                neighbor->setStatus(
+                    MAZE_STATUS_VISITED);  // Marquage comme visitée
                 neighbor->setAlreadyVisited(true);
             }
         }
     }
 
-    refreshShow(show);
+    refreshShow(show);  // Rafraîchissement final de l'affichage
+
+    // Vérification si toutes les cellules ont été visitées
     for (int i = 0; i < maze->getWidth(); i++) {
         for (int j = 0; j < maze->getHeight(); j++) {
             const Cell *cell = maze->getCell(i, j);
             if (!cell->isAlreadyVisited()) {
                 if (isValid != nullptr) *isValid = false;
+                // Libération de la mémoire allouée dynamiquement
                 while (!queueFree.empty()) {
                     const auto *temp =
                         static_cast<positionHistory *>(queueFree.front());
@@ -75,7 +98,10 @@ void checker_breadth_first(const Maze *maze, const bool perfect, Show *show,
             }
         }
     }
-    if (isValid != nullptr) *isValid = true;
+
+    if (isValid != nullptr) *isValid = true;  // Le labyrinthe est valide.
+
+    // Vérification si le labyrinthe est parfait
     if (perfect) {
         if (!imperfect) {
             if (isPerfect != nullptr) *isPerfect = true;
@@ -83,6 +109,8 @@ void checker_breadth_first(const Maze *maze, const bool perfect, Show *show,
             if (isPerfect != nullptr) *isPerfect = false;
         }
     }
+
+    // Libération de la mémoire allouée dynamiquement
     while (!queueFree.empty()) {
         const auto *temp = static_cast<positionHistory *>(queueFree.front());
         if (temp != &startHistory) delete temp;
--- src/lib/checker/depth_first.cpp
+++ src/lib/checker/depth_first.cpp
@@ -4,6 +4,8 @@
 #include "../maze.hpp"
 #include "../stack.hpp"
 
+// Structure pour stocker l'historique des positions visitées, la direction et
+// leur parent
 struct positionHistory {
     int x;
     int y;
@@ -11,27 +13,40 @@ struct positionHistory {
     positionHistory *parent;
 };
 
+// Fonction principale pour vérifier le labyrinthe en utilisant un parcours en
+// profondeur (DFS)
 void checker_depth_first(const Maze *maze, const bool perfect, const bool left,
                          Show *show, bool *isValid, bool *isPerfect) {
-    Stack stack;
-    auto stackFree = Stack();
-    bool imperfect = false;
+    Stack stack;      // Pile pour gérer le parcours DFS
+    Stack stackFree;  // Pile pour gérer la libération de la mémoire
+    bool imperfect =
+        false;  // Drapeau pour vérifier si le labyrinthe est parfait
+
+    // Vérification des cellules de départ et d'arrivée
     if (maze->getStartCell() == nullptr || maze->getEndCell() == nullptr) {
         return;
     }
-    refreshShow(show);
+
+    refreshShow(show);  // Rafraîchissement initial de l'affichage
+
+    // Initialisation du point de départ
     Cell *start = maze->getCell(0, 0);
     positionHistory startHistory = {0, 0, 0, nullptr};
-    stack.push(&startHistory);
-    start->setStatus(MAZE_STATUS_VISITED);
+    stack.push(&startHistory);  // Ajout de la position initiale à la pile
+    start->setStatus(
+        MAZE_STATUS_VISITED);  // Marquage de la cellule de départ comme visitée
     start->setAlreadyVisited(true);
 
+    // Parcours en profondeur (DFS)
     while (!stack.empty()) {
+        // Extraction de l'élément au sommet de la pile
         auto *current = static_cast<positionHistory *>(stack.top());
         stack.pop();
         const int x = current->x;
         const int y = current->y;
         Cell *cell = maze->getCell(x, y);
+
+        // Vérification si le labyrinthe n'est pas parfait
         if (cell != nullptr && perfect &&
             cell->getAbsoluteNumberOfNeighbors() -
                     cell->getAbsoluteNumberOfNeighborsNotVisited() >=
@@ -39,35 +54,49 @@ void checker_depth_first(const Maze *maze, const bool perfect, const bool left,
             cell->setStatus(MAZE_STATUS_TOO_MANY_NEIGHBORS);
             imperfect = true;
         }
+
+        // Vérification des voisins non visités
         if (cell->getAbsoluteNumberOfNeighborsNotVisited() == 0) {
             if (cell->getAbsoluteNumberOfNeighbors() -
                     cell->getAbsoluteNumberOfNeighborsNotVisited() <
                 2) {
                 cell->setStatus(MAZE_STATUS_HOPELESS);
             }
-            refreshShow(show, 1, &cell, true);
+            refreshShow(show, 1, &cell,
+                        true);  // Mise à jour de l'affichage pour cette cellule
         } else {
-            refreshShow(show, 1, &cell, false);
+            refreshShow(show, 1, &cell,
+                        false);  // Mise à jour normale de l'affichage
         }
+
+        // Exploration des voisins
         for (int i = 0; i < 4; i++) {
-            const int index = (current->direction + left ? i : i + 2) % 4;
+            // Indice du voisin ajusté en fonction de la direction et du
+            // paramètre 'left'
+            const int index = (current->direction + (left ? i : i + 2)) % 4;
             Cell *neighbor = cell->getNeighbor(index);
             if (neighbor != nullptr && !neighbor->isAlreadyVisited()) {
                 auto *next = new positionHistory{
+                    // Création d'un nouvel élément d'historique
                     neighbor->getX(), neighbor->getY(), index, current};
-                stack.push(next);
-                stackFree.push(next);
-                neighbor->setStatus(MAZE_STATUS_VISITED);
+                stack.push(next);      // Ajout à la pile principale
+                stackFree.push(next);  // Ajout à la pile pour la libération
+                neighbor->setStatus(
+                    MAZE_STATUS_VISITED);  // Marquage comme visitée
                 neighbor->setAlreadyVisited(true);
             }
         }
     }
-    refreshShow(show);
+
+    refreshShow(show);  // Rafraîchissement final de l'affichage
+
+    // Vérification si toutes les cellules ont été visitées
     for (int i = 0; i < maze->getWidth(); i++) {
         for (int j = 0; j < maze->getHeight(); j++) {
             const Cell *cell = maze->getCell(i, j);
             if (!cell->isAlreadyVisited()) {
                 if (isValid != nullptr) *isValid = false;
+                // Libération de la mémoire allouée dynamiquement
                 while (!stackFree.empty()) {
                     const auto *temp =
                         static_cast<positionHistory *>(stackFree.top());
@@ -78,7 +107,10 @@ void checker_depth_first(const Maze *maze, const bool perfect, const bool left,
             }
         }
     }
-    if (isValid != nullptr) *isValid = true;
+
+    if (isValid != nullptr) *isValid = true;  // Le labyrinthe est valide.
+
+    // Vérification si le labyrinthe est parfait
     if (perfect) {
         if (!imperfect) {
             if (isPerfect != nullptr) *isPerfect = true;
@@ -86,6 +118,8 @@ void checker_depth_first(const Maze *maze, const bool perfect, const bool left,
             if (isPerfect != nullptr) *isPerfect = false;
         }
     }
+
+    // Libération de la mémoire allouée dynamiquement
     while (!stackFree.empty()) {
         const auto *temp = static_cast<positionHistory *>(stackFree.top());
         if (temp != &startHistory) delete temp;
